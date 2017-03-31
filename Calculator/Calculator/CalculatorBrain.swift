f553d94f0781b7d3ca9d680e05e5771d7f8457d2
Ling Tran <lxiutran@gmail.com>
2017-03-31 15:40:54 -0500

    iOS 101 2 | Add remaining BinaryOperations with use of super cool closures

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-func multiply (op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
-
 class CalculatorBrain {
     
     private var accumulator = 0.0
@@ -25,7 +21,10 @@ class CalculatorBrain {
         "e": Operation.Constant(M_E),
         "√": Operation.UnaryOperation(sqrt),
         "cos": Operation.UnaryOperation(cos),
-        "×": Operation.BinaryOperation(multiply),
+        "×": Operation.BinaryOperation({ $0 * $1 }),
+        "÷": Operation.BinaryOperation({ $0 / $1 }),
+        "+": Operation.BinaryOperation({ $0 + $1 }),
+        "-": Operation.BinaryOperation({ $0 - $1 }),
         "=": Operation.Equals
     ]
     
