3472debe794dc6e1000703cf0c970e05dce1277f
Ling Tran <lxiutran@gmail.com>
2017-03-31 15:45:59 -0500

    iOS 101 2 | Add plus-minus operation

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -19,6 +19,7 @@ class CalculatorBrain {
     var operations: Dictionary<String, Operation> = [
         "π": Operation.Constant(Double.pi),
         "e": Operation.Constant(M_E),
+        "±": Operation.UnaryOperation({ -$0 }),
         "√": Operation.UnaryOperation(sqrt),
         "cos": Operation.UnaryOperation(cos),
         "×": Operation.BinaryOperation({ $0 * $1 }),
