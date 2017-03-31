7ed5b240dc7e5f27c079f5221a741e62d9020ac1
Ling Tran <lxiutran@gmail.com>
2017-03-31 10:16:42 -0500

    Add conditional to display correct numbers in calculator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping: Bool = false
     
     @IBAction func touchDigit(_ sender: UIButton ) {
         let digit = sender.currentTitle!
-        let textCurrentlyInDisplay = display!.text!
-        display!.text = textCurrentlyInDisplay + digit
-     
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+
+        userIsInTheMiddleOfTyping = true
         
     }
 
